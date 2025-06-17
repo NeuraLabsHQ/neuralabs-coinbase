37b96b6f910d9b425d3bad951066a6e7b038b1a8
Harsh <harshvpoddar1605@gmail.com>
2025-06-17 10:09:35 +0530

    Remove outdated integration tests and related configuration files for NeuraLabs NFT contract; streamline testing process by deleting unnecessary scripts and test configurations.

--- mov_contract/frontend_interaction/src/blockchain_module/nfts/index.ts
+++
@@ -1,9 +0,0 @@
-// NFT module main export
-
-export { mintNFT, batchMintNFTs } from './create';
-export type { MintNFTParams } from './create';
-
-export { getUserNFTs, getNFTById, getTotalNFTCount } from './query';
-
-// Re-export NFT types
-export type { NFTData } from '../types';
